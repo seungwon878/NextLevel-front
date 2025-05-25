9885b08ebb8d14e297ad4a451ae4971ac5712ae3
seungwon <dasusam84@gmail.com>
2025-05-25 23:47:18 +0900

    a

---
+++ src/Routes/routes.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { RouteObject } from 'react-router-dom';
+import { LandingPageContainer } from "../Pages/LandingPage";
+
+export const routes: RouteObject[] = [
+    {
+      path: "/",
+      element: <LandingPageContainer />,
+    },
+]
+
+
